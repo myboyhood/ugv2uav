003b331841eba06a089575bd8d7c5f4d181fb6d5
myboyhood <2390441535@qq.com>
2020-05-06 17:44:00 +0800

    env1

---
+++ rotors_simulator/rotors_gazebo/cmake-build-debug/CMakeFiles/designed_waypoint_publisher.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for designed_waypoint_publisher.
+# This may be replaced when dependencies are built.
---
+++ rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/DependInfo.cmake
@@ -0,0 +1,119 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Actuators.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/Actuators.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CommandMotorSpeed.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/CommandMotorSpeed.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/ConnectGazeboToRosTopic.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/ConnectGazeboToRosTopic.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/ConnectRosToGazeboTopic.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/ConnectRosToGazeboTopic.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Float32.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/Float32.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/FluidPressure.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/FluidPressure.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Header.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/Header.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Imu.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/Imu.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/JointState.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/JointState.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Lidar.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/Lidar.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/MagneticField.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/MagneticField.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/NavSatFix.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/NavSatFix.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Odometry.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/Odometry.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/OpticalFlow.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/OpticalFlow.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Point.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/Point.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/PointStamped.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/PointStamped.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/PoseStamped.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/PoseStamped.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/PoseWithCovariance.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/PoseWithCovariance.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/PoseWithCovarianceStamped.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/PoseWithCovarianceStamped.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/RollPitchYawrateThrust.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/RollPitchYawrateThrust.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Transform.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/Transform.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/TransformStamped.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/TransformStamped.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/TransformStampedWithFrameIds.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/TransformStampedWithFrameIds.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Twist.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/Twist.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/TwistStamped.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/TwistStamped.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/TwistWithCovariance.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/TwistWithCovariance.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Vector3dStamped.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/Vector3dStamped.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/WindSpeed.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/WindSpeed.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Wrench.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/Wrench.pb.cc.o"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/WrenchStamped.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CMakeFiles/mav_msgs.dir/WrenchStamped.pb.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LIBBULLET_VERSION=2.87"
+  "LIBBULLET_VERSION_GT_282"
+  "MAVLINK_INTERFACE=TRUE"
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_ASSERT_ENABLED"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"rotors_gazebo_plugins\""
+  "mav_msgs_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/gazebo-9"
+  "/usr/include/bullet"
+  "/usr/include/simbody"
+  "/usr/include/sdformat-6.0"
+  "/usr/include/ignition/math4"
+  "/usr/include/OGRE"
+  "/usr/include/OGRE/Terrain"
+  "/usr/include/OGRE/Paging"
+  "/usr/include/ignition/transport4"
+  "/usr/include/ignition/msgs1"
+  "/usr/include/ignition/common1"
+  "/usr/include/ignition/fuel_tools1"
+  "/usr/include/opencv"
+  "."
+  "../include"
+  "/home/wzy/catkin_ws/devel/include"
+  "/home/wzy/catkin_ws/src/mav_comm/mav_msgs/include"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_control/include"
+  "/opt/ros/melodic/include"
+  "/opt/ros/melodic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  "/usr/include/eigen3"
+  "/usr/include/gazebo-9/gazebo/msgs"
+  "/home/wzy/mavros_ws/devel/include/mavlink/v2.0"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Actuators.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Actuators.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CommandMotorSpeed.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/CommandMotorSpeed.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/ConnectGazeboToRosTopic.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/ConnectGazeboToRosTopic.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/ConnectRosToGazeboTopic.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/ConnectRosToGazeboTopic.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Float32.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Float32.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/FluidPressure.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/FluidPressure.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Header.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Header.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Imu.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Imu.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/JointState.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/JointState.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Lidar.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Lidar.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/MagneticField.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/MagneticField.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/NavSatFix.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/NavSatFix.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Odometry.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Odometry.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/OpticalFlow.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/OpticalFlow.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Point.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Point.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/PointStamped.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/PointStamped.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/PoseStamped.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/PoseStamped.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/PoseWithCovariance.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/PoseWithCovariance.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/PoseWithCovarianceStamped.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/PoseWithCovarianceStamped.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/RollPitchYawrateThrust.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/RollPitchYawrateThrust.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Transform.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Transform.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/TransformStamped.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/TransformStamped.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/TransformStampedWithFrameIds.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/TransformStampedWithFrameIds.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Twist.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Twist.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/TwistStamped.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/TwistStamped.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/TwistWithCovariance.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/TwistWithCovariance.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Vector3dStamped.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Vector3dStamped.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/WindSpeed.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/WindSpeed.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Wrench.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/Wrench.pb.h"
+  "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/WrenchStamped.pb.cc" "/home/wzy/catkin_ws/src/rotors_simulator/rotors_gazebo_plugins/cmake-build-debug/WrenchStamped.pb.h"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
